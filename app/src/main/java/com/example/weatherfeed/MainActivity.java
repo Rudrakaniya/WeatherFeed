b20cf2dd69868b865c9b32dd0cc3ae988554902a
Rudra kaniya <rudra.kaniya.rk@gmail.com>
2020-07-25 14:43:08 +0530

    added elevation to the navigation drawer

--- app/src/main/java/com/example/weatherfeed/MainActivity.java
+++ app/src/main/java/com/example/weatherfeed/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.weatherfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout navConstraintLayout = findViewById(R.id.navigationDrawer);
+        navConstraintLayout.bringToFront();
     }
 }
