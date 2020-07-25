651b8b4a9cae1b134e27cb562b10410f0dc2df90
Rudra kaniya <rudra.kaniya.rk@gmail.com>
2020-07-25 20:35:57 +0530

    added back-press functionality

--- app/src/main/java/com/example/weatherfeed/MainActivity.java
+++ app/src/main/java/com/example/weatherfeed/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.weatherfeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    MotionLayout mMotionLayout;
+    ImageButton mImageButton;
+    boolean isDrawerOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMotionLayout = findViewById(R.id.motion_layout);
+        mImageButton = findViewById(R.id.img);
 
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isDrawerOpen) {
+                    mMotionLayout.transitionToEnd();
+                    isDrawerOpen = true;
+                } else {
+                    mMotionLayout.transitionToStart();
+                    isDrawerOpen = false;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!isDrawerOpen) {
+            super.onBackPressed();
+            return;
+        }
+        mMotionLayout.transitionToStart();
+        isDrawerOpen = false;
     }
 }
