d4b3c6692542acb8c746ae7c44f82d98c6a95001
Rudra kaniya <rudra.kaniya.rk@gmail.com>
2020-08-22 23:54:33 +0300

    Removed unwanted files

--- app/src/main/java/com/example/weatherfeed/SliderAdapterExample.java
+++ app/src/main/java/com/example/weatherfeed/SliderAdapterExample.java
@@ -2,12 +2,10 @@ package com.example.weatherfeed;
 
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
